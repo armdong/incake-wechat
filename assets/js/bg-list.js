8747c68db310ce698e8eef6f3cd0241f5872223e
chenxj92 <842674122@qq.com>
2016-01-28 17:59:28 +0800

    添加播放事件

--- assets/js/bg-list.js
+++ assets/js/bg-list.js
@@ -1,73 +1,91 @@
-(function(){
-	
-	$(function(){	
-		
-		//play or paush
-		fnPlayPause();
-		//check voice
-		fnInitVoice();
-		//submit
-		fnBtnSubmit();
-		
-	});
-	
-	//play or paush
-	function fnPlayPause(){
-		var $aItemPlay = $('.item-play');
-		var $aItemPause = $('.item-pause')
-		
-		//play
-		$.each($aItemPlay, function(i,ele) {			
-			$(ele).on('click',function(){		
-				
-				$aItemPlay.css('display','block');
-				$aItemPause.css('display','none');
-				
-				$(this).css('display','none');
-				$aItemPause.eq(i).css('display','block');
-			});
-		});	
-		
-		//paush
-		$.each($aItemPause, function(i,ele) {			
-			$(ele).on('click',function(){				
-				$(this).css('display','none');
-				$aItemPlay.eq(i).css('display','block');;
-			});
-		});	
-		
-	}
-	
-	//check voice
-	function fnInitVoice(){
+(function() {
+
+	$(function() {
+
 		var $oUl = $('.bg-list');
 		var $aUlLi = $oUl.find('.bg-item');
 		var $aItemText = $aUlLi.find('.item-text');
-		
-		//check model
-		$.each($aItemText, function(i,ele) {			
-			$(ele).on('click',function(){				
-				$aUlLi.removeClass("active");
-				$aUlLi.eq(i).addClass("active");
-			});
-		});		
+		var arrVioce = ['assets/audio/bgm.mp3', 'assets/audio/hello.mp3', 'assets/audio/juice.mp3', 'assets/audio/xindong.mp3'];
+		var $oPlayer = $('#player');
+		var $aItemPlay = $('.item-play');
+		var $aItemPause = $('.item-pause');
 
-	}
-	
-	//submit
-	function fnBtnSubmit(){
-		var $popupTip = $('.popup-tip');
-		var $btnSure = $('.btn-sure');
-		var $btnReturn = $('.btn-return');
-		
-		$btnSure.on('click',function(){
-			$popupTip.removeClass("hide");
-		});
-		
-		$btnReturn.on('click',function(){
-			$popupTip.addClass("hide");
-		});
-		
-	}
-	
+		//check voice
+		fnInitVoice();
+		//play or paush
+		fnPlayPause();
+		//submit
+		fnBtnSubmit();
+
+		//check voice
+		function fnInitVoice() {
+
+			//check model
+			$.each($aItemText, function(i, ele) {
+				$(ele).on('click', function() {
+					//check this
+					$aUlLi.removeClass("active");
+					$aUlLi.eq(i).addClass("active");
+					//hide all pause
+					$aItemPlay.css('display', 'block');
+					$aItemPause.css('display', 'none');
+					// player pause
+					$oPlayer.get(0).pause();
+				});
+			});
+
+		}
+
+
+		//play or paush
+		function fnPlayPause() {
+			var originSrc = '';
+			//play
+			$.each($aItemPlay, function(i, ele) {
+				$(ele).on('click', function() {
+					// check model
+					$aItemText.eq(i + 1).trigger('click');
+					// get player src
+					originSrc = $oPlayer.attr('src');
+					//show this pause
+					$(this).css('display', 'none');
+					$aItemPause.eq(i).css('display', 'block');
+					// make sure play or pause
+					if (originSrc !== arrVioce[i]) {
+						$oPlayer.attr('src', arrVioce[i])
+					}
+					// player begin
+					$oPlayer.get(0).play();
+				});
+			});
+
+			//paush
+			$.each($aItemPause, function(i, ele) {
+				$(ele).on('click', function() {
+					$(this).css('display', 'none');
+					$aItemPlay.eq(i).css('display', 'block');
+					// player pause
+					$oPlayer.get(0).pause();
+				});
+			});
+
+		}
+
+		//submit
+		function fnBtnSubmit() {
+			var $popupTip = $('.popup-tip');
+			var $btnSure = $('.btn-sure');
+			var $btnReturn = $('.btn-return');
+
+			$btnSure.on('click', function() {
+				$popupTip.removeClass("hide");
+			});
+
+			$btnReturn.on('click', function() {
+				$popupTip.addClass("hide");
+			});
+
+		}
+	});
+
 })();
--- assets/js/my-bg.js
+++ assets/js/my-bg.js
@@ -1,52 +1,92 @@
-(function(){
-	
-	$(function(){	
-		fnInitVoice();
-	});
-	
-	function fnInitVoice(){
+(function() {
+
+	$(function() {
 		var $oUl = $('.voice-list');
 		var $aUlLi = $oUl.find('.voice-item');
 		var $aItemText = $aUlLi.find('.item-text');
 		var $aBtnDel = $aUlLi.find('.item-del');
+		var arrVioce = ['assets/audio/bgm.mp3', 'assets/audio/hello.mp3', 'assets/audio/xindong.mp3'];
+		var $oPlayer = $('#player');
 		var $aItemPlay = $('.item-play');
 		var $aItemPause = $('.item-pause')
 		
-		//check model
-		$.each($aItemText, function(i,ele) {			
-			$(ele).on('click',function(){				
-				$aUlLi.removeClass("active");
-				$aUlLi.eq(i).addClass("active");
-			});
-		});		
+		//check voice
+		fnInitVoice();
+		//play or paush
+		fnPlayPause();
+		//delete model
+		fnDeleteVoice();
 
+		function fnInitVoice() {
+			//check model
+			$.each($aItemText, function(i, ele) {
+				$(ele).on('click', function() {
+					//check this
+					$aUlLi.removeClass("active");
+					$aUlLi.eq(i).addClass("active");
+					//hide all pause
+					$aItemPlay.css('display', 'block');
+					$aItemPause.css('display', 'none');
+					// player pause
+					$oPlayer.get(0).pause();
+				});
+			});
+
+			// delete model
+			$.each($aBtnDel, function(i, ele) {
+				$(ele).on('click', function() {
+					$aUlLi.eq(i).remove();
+				});
+			});
+		}
+		
+		//play or paush
+		function fnPlayPause() {
+			var originSrc = '';
+			//play
+			$.each($aItemPlay, function(i, ele) {
+				$(ele).on('click', function() {
+					// check model
+					$aItemText.eq(i).trigger('click');
+					// get player src
+					originSrc = $oPlayer.attr('src');
+					
+					//show this pause
+					$(this).css('display', 'none');
+					$aItemPause.eq(i).css('display', 'block');
+					// make sure play or pause
+					if (originSrc !== arrVioce[i]) {
+						$oPlayer.attr('src', arrVioce[i])
+					}
+					// player begin
+					$oPlayer.get(0).play();
+				});
+			});
+
+			//paush
+			$.each($aItemPause, function(i, ele) {
+				$(ele).on('click', function() {
+					$(this).css('display', 'none');
+					$aItemPlay.eq(i).css('display', 'block');
+					// player pause
+					$oPlayer.get(0).pause();
+				});
+			});
+		}
+		
 		// delete model
-		$.each($aBtnDel, function(i,ele) {			
-			$(ele).on('click',function(){				
-				$aUlLi.eq(i).remove();				
-			});			
-		});	
-		
-		//play
-		$.each($aItemPlay, function(i,ele) {			
-			$(ele).on('click',function(){		
-				
-				$aItemPlay.css('display','block');
-				$aItemPause.css('display','none');
-				
-				$(this).css('display','none');
-				$aItemPause.eq(i).css('display','block');
+		function fnDeleteVoice() {
+			$.each($aBtnDel, function(i, ele) {
+				$(ele).on('click', function() {
+					$aUlLi.eq(i).remove();
+					// player pause
+					$oPlayer.get(0).pause();
+					// remove voice
+					$(arrVioce).splice(i,1);
+				});
 			});
-		});	
+		}
 		
-		//paush
-		$.each($aItemPause, function(i,ele) {			
-			$(ele).on('click',function(){				
-				$(this).css('display','none');
-				$aItemPlay.eq(i).css('display','block');;
-			});
-		});	
-		
-	}
-	
+	});
+
 })();
