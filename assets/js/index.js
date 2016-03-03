37b6dfeef39473e0aceb3cd7c9ca7989cdf5670f
armdong <armdong@163.com>
2016-03-03 11:39:40 +0800

    添加搜索功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,129 +5,153 @@
  */
 (function() {
 
-	$(function() {
+    $(function() {
 
-		var $aImg = $('#index-swiper').find('img');
-		var swiper = null;
-		var containerWidth = 0;
-		// 图片懒加载
-		new LazyLoad();
+        var $aImg = $('#index-swiper').find('img');
+        var swiper = null;
+        var containerWidth = 0;
+        // 图片懒加载
+        new LazyLoad();
 
-		fnInitSwiper();
-		fnInitCity();
-		fnAdoptNav();
-		fnAdoptFavours();
+        fnInitSwiper();
+        fnInitCity();
+        fnAdoptNav();
+        fnAdoptFavours();
 
-		$(window).on('resize', function() {
-			fnInitSwiper();
-			fnInitCity();
-			fnAdoptNav();
-			fnAdoptFavours();
-		});
+        $(window).on('resize', function() {
+            fnInitSwiper();
+            fnInitCity();
+            fnAdoptNav();
+            fnAdoptFavours();
+        });
 
-		function fnInitSwiper() {
-			containerWidth = $('#index-swiper').width();
-			$aImg.css({
-				width: containerWidth,
-				height: containerWidth / 2
-			});
-			if (swiper != null) {
-				swiper.destroy();
-			}
-			swiper = null;
-			swiper = new Swiper('.swiper-container', {
-				pagination: '.swiper-pagination',
-				width: containerWidth,
-				height: containerWidth / 2,
-				loop: true,
-				centeredSlides: true,
-				autoplay: 5000,
-				speed: 1000,
-				autoplayDisableOnInteraction: false,
-				preloadImages: false,
-				lazyLoading: true
-			});
-		}
+        var clientH = $(window).height(),
+            $oHeader = $('#header'),
+            iHearderH = $oHeader.height(),
+            $oLogo = $oHeader.find('.logo'),
+            $oSearch = $oHeader.find('.search'),
+            $oSearchInput = $oSearch.find('.txt-search');
 
-		function fnInitCity() {
+        $(window).on('scroll', function() {
+            var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+            if (scrollT >= iHearderH) {
+                $oLogo.hide();
+                $oSearch.show();
+            } else {
+                $oLogo.show();
+                $oSearch.hide();
+            }
+        });
 
-			// 城市选择
-			$('.location').on('click', function() {
-				var $oMask = $('#mask');
-				if ($oMask.hasClass('hide')) {
-					$oMask.removeClass('hide');
-					// 禁用页面touchmove
-					$('body').bind('touchmove', function(e) {
-						e.preventDefault();
-					});
-					fnFitCity();
-				}
-			});
+        // 搜索框获取焦点跳转
+        $oSearchInput.on('focus', function() {
+            window.location.href = 'search.html';
+        });
 
-			// 城市选择确认
-			$('#btnCitySure').on('click', function() {
-				var $oMask = $('#mask');
-				if (!$oMask.hasClass('hide')) {
-					$oMask.addClass('hide');
-					// 禁用页面touchmove
-					$('body').unbind('touchmove');
-				}
-			});
+        function fnInitSwiper() {
+            containerWidth = $('#index-swiper').width();
+            $aImg.css({
+                width: containerWidth,
+                height: containerWidth / 2
+            });
+            if (swiper != null) {
+                swiper.destroy();
+            }
+            swiper = null;
+            swiper = new Swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                width: containerWidth,
+                height: containerWidth / 2,
+                loop: true,
+                centeredSlides: true,
+                autoplay: 5000,
+                speed: 1000,
+                autoplayDisableOnInteraction: false,
+                preloadImages: false,
+                lazyLoading: true
+            });
+        }
 
-			// 城市切换
-			var $oCity = $('#city-group');
-			var $aCitySpan = $oCity.find('span');
-			
-			$.each($aCitySpan, function(i, ele) {
-				$(ele).on('click', function() {
-					$(this).siblings().removeClass('active');
-					$(this).addClass('active');
-				});
-			});
-			
-			function fnFitCity(){
-				var $oCity = $('#city-group');
-				var $aCitySpan = $oCity.find('span');
-				var iCityW = $oCity.width();
-				var iCitySpanW = $aCitySpan.width();			
-				var iMargin = Math.floor((iCityW - iCitySpanW * 4 - 8)/8);
-				
-				$.each($aCitySpan, function(i, ele) {
-					$(ele).css({
-						'margin-left':iMargin+'px'
-					});
-				});
-			}
-		}
+        function fnInitCity() {
 
-		// 菜单自适应
-		function fnAdoptNav() {
-			var $oCategory = $('#nav-category');
-			var $aCategoryLi = $oCategory.find('>li');
-			var iLen = $aCategoryLi.length;
-			var iCategoryWidth = $oCategory.width();
-			var iWidth = $aCategoryLi.width();
+            // 城市选择
+            $('.location').on('click', function() {
+                var $oMask = $('#mask');
+                if ($oMask.hasClass('hide')) {
+                    $oMask.removeClass('hide');
+                    // 禁用页面touchmove
+                    $('body').bind('touchmove', function(e) {
+                        e.preventDefault();
+                    });
+                    fnFitCity();
+                }
+            });
 
-			// 根据菜单项的个数来布局
-			$.each($aCategoryLi, function(i, ele) {
-				$(ele).css({
-					'left': i * (iCategoryWidth / iLen) + 'px',
-					'margin-left': ((iCategoryWidth / iLen) - iWidth) / 2 + 'px'
-				});
-			});
-			$oCategory.css({'opacity':'1'});
-		}
+            // 城市选择确认
+            $('#btnCitySure').on('click', function() {
+                var $oMask = $('#mask');
+                if (!$oMask.hasClass('hide')) {
+                    $oMask.addClass('hide');
+                    // 禁用页面touchmove
+                    $('body').unbind('touchmove');
+                }
+            });
 
-		// 猜你喜欢
-		function fnAdoptFavours() {
-			var $oFavours = $('.favours-list');
-			var $aFavoursLi = $oFavours.find('>li');
-			$.each($aFavoursLi, function(i, ele) {
-				$(ele).css({
-					'float': i % 2 == 0 ? 'left' : 'right'
-				});
-			});
-		}
-	});
+            // 城市切换
+            var $oCity = $('#city-group');
+            var $aCitySpan = $oCity.find('span');
+
+            $.each($aCitySpan, function(i, ele) {
+                $(ele).on('click', function() {
+                    $(this).siblings().removeClass('active');
+                    $(this).addClass('active');
+                });
+            });
+
+            function fnFitCity() {
+                var $oCity = $('#city-group');
+                var $aCitySpan = $oCity.find('span');
+                var iCityW = $oCity.width();
+                var iCitySpanW = $aCitySpan.width();
+                var iMargin = Math.floor((iCityW - iCitySpanW * 4 - 8) / 8);
+
+                $.each($aCitySpan, function(i, ele) {
+                    $(ele).css({
+                        'margin-left': iMargin + 'px'
+                    });
+                });
+            }
+        }
+
+        // 菜单自适应
+        function fnAdoptNav() {
+            var $oCategory = $('#nav-category');
+            var $aCategoryLi = $oCategory.find('>li');
+            var iLen = $aCategoryLi.length;
+            var iCategoryWidth = $oCategory.width();
+            var iWidth = $aCategoryLi.width();
+
+            // 根据菜单项的个数来布局
+            $.each($aCategoryLi, function(i, ele) {
+                $(ele).css({
+                    'left': i * (iCategoryWidth / iLen) + 'px',
+                    'margin-left': ((iCategoryWidth / iLen) - iWidth) / 2 + 'px'
+                });
+            });
+            $oCategory.css({ 'opacity': '1' });
+        }
+
+        // 猜你喜欢
+        function fnAdoptFavours() {
+            var $oFavours = $('.favours-list');
+            var $aFavoursLi = $oFavours.find('>li');
+            $.each($aFavoursLi, function(i, ele) {
+                $(ele).css({
+                    'float': i % 2 == 0 ? 'left' : 'right'
+                });
+            });
+        }
+
+    });
 
 })();
--- assets/js/search.js
+++ assets/js/search.js
@@ -7,32 +7,12 @@
 	
 	// 搜索
 	function fnSearch(){
-		var $oTxtSearch = $('.txt-search');
 		var $oBtnSearch = $('.btn-search');
-		var $oHisBody = $('.history-body');
 		
 		$oBtnSearch.on('click',function(){
-			var txt = $oTxtSearch.val();
-			var _html = '';
-			_html += '<span>';
-			_html += txt;
-			_html += '</span>';
-			
+						
 			//TODO 入库操作
-			var $aSpan = $oHisBody.find('span');
-			if( $aSpan.length > 0 ){
-				// 判断是否有重复项				
-				$.each($aSpan, function(i,ele) {
-					var currTxt = $(ele).html();
-					if(currTxt === txt){
-						$(ele).remove();
-					}
-				});				
-				$oHisBody.prepend(_html);
-			}else{
-				$oHisBody.html(_html);
-			}
-			$oTxtSearch.val('');
+			window.location.href = 'search-list.html';			
 		});
 	}
 	
