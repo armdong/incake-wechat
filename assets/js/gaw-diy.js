d0312e97b939118aaacc660145b1bc0974b9f003
armdong <armdong@163.com>
2016-08-10 18:52:39 +0800

    图片上传

--- assets/js/gaw-diy.js
+++ assets/js/gaw-diy.js
@@ -1,81 +1,110 @@
-(function(window, document){
+(function(window, document) {
 
-	$(function(){
+    $(function() {
 
-		var $oForm = $('#frmApply'),
-			$oSelCategory = $oForm.find('#selCategories'),
-			$oSelColor = $oForm.find('#selColors'),
-			$oMaskCategory = $oForm.find('.mask-categoryPop'),
-			$oMaskCategoryClose = $oMaskCategory.find('.mask-close'),
-			$oMaskColor = $oForm.find('.mask-colorPop'),
-			$oMaskColorClose = $oMaskColor.find('.mask-close'),
-			$oTextarea = $oForm.find('#txtSummary'),
-			$oLastWord = $oForm.find('#lastWord'),
-			$aPhoto = $oForm.find('.photo'),
-			$aBtnClose = $oForm.find('.btn-close'),
-			$oLastPhoto = $oForm.find('#lastPhoto'),
-			$oMaskPhoto = $oForm.find('.mask-photoPop'),
-			$oPhotoPop = $oForm.find('.photoPop'),
-			iMaxLen = 800;
+        var $oForm = $('#frmApply'),
+            $oSelCategory = $oForm.find('#selCategories'),
+            $oSelColor = $oForm.find('#selColors'),
+            $oMaskCategory = $oForm.find('.mask-categoryPop'),
+            $oMaskCategoryClose = $oMaskCategory.find('.mask-close'),
+            $oMaskColor = $oForm.find('.mask-colorPop'),
+            $oMaskColorClose = $oMaskColor.find('.mask-close'),
+            $oTextarea = $oForm.find('#txtSummary'),
+            $oLastWord = $oForm.find('#lastWord'),
+            $oLastPhoto = $oForm.find('#lastPhoto'),
+            $oMaskPhoto = $oForm.find('.mask-photoPop'),
+            $oPhotoPop = $oForm.find('.photoPop'),
+            iMaxLen = 800;
 
-		// 种类下拉框改变事件
-		$oSelCategory.on('change', function(e){
-			var val = $(this).val();
-			if (val === '3') {	// 多层
-				$oMaskCategory.fadeIn();
-			}
-		});
+        var $oUpload = $oForm.find('.upload'),
+            $oBtnAdd = $oUpload.find('.btn-add');
 
-		// 关闭种类下拉框
-		$oMaskCategoryClose.on('click', function(e){
-			$oSelCategory.val('0');	// 设置第一项为选中
-			$oMaskCategory.fadeOut();
-		});
+        // 种类下拉框改变事件
+        $oSelCategory.on('change', function(e) {
+            var val = $(this).val();
+            if (val === '3') { // 多层
+                $oMaskCategory.fadeIn();
+            }
+        });
 
-		// 颜色下拉框改变事件
-		$oSelColor.on('change', function(e){
-			var val = $(this).val();
-			if (val === '7') {	// 其它
-				$oMaskColor.fadeIn();
-			}
-		});
+        // 关闭种类下拉框
+        $oMaskCategoryClose.on('click', function(e) {
+            $oSelCategory.val('0'); // 设置第一项为选中
+            $oMaskCategory.fadeOut();
+        });
 
-		// 关闭颜色下拉框
-		$oMaskColorClose.on('click', function(e){
-			$oSelColor.val('0');	// 设置第一项为选中
-			$oMaskColor.fadeOut();
-		});
+        // 颜色下拉框改变事件
+        $oSelColor.on('change', function(e) {
+            var val = $(this).val();
+            if (val === '7') { // 其它
+                $oMaskColor.fadeIn();
+            }
+        });
 
-		$oTextarea.bind('input propertychange', function(e){
-			var text = $(this).val(),
-        		iLen = text.length;
+        // 关闭颜色下拉框
+        $oMaskColorClose.on('click', function(e) {
+            $oSelColor.val('0'); // 设置第一项为选中
+            $oMaskColor.fadeOut();
+        });
 
-        	if (iLen > iMaxLen) {
-        		$oTextarea.val(text.substring(0, iMaxLen));
-        		iLen = iMaxLen;
+        $oTextarea.bind('input propertychange', function(e) {
+            var text = $(this).val(),
+                iLen = text.length;
+
+            if (iLen > iMaxLen) {
+                $oTextarea.val(text.substring(0, iMaxLen));
+                iLen = iMaxLen;
+            }
+            $oLastWord.html(iMaxLen - iLen);
+        });
+
+        //预览选择图片
+        $oUpload.on('click', '.photo', function(){
+        	$oMaskPhoto.fadeIn();
+        });
+
+        //取消预览
+        $oPhotoPop.on('click', function(e) {
+            $oMaskPhoto.fadeOut();
+        });
+
+        //删除选择图片
+        $oUpload.on('click', '.btn-close', function(){
+        	$(this).closest('.photos').remove();
+            var number = parseInt($oLastPhoto.text()) + 1; //更改可上传图片数量
+            $oLastPhoto.text(number);
+            if($oUpload.find('.photo').length < 3){
+        		$oBtnAdd.show();
         	}
-        	$oLastWord.html(iMaxLen - iLen);
-		});
-		
-		//预览选择图片
-		$aPhoto.on('click',function(e){
-			//var src = $(this).children().attr("src"); //此处获得选中图片的预览图地址
-			//$oPhotoPop.children().attr('src',src);
-			$oMaskPhoto.fadeIn();
-		});
-		
-		//取消预览
-		$oPhotoPop.on('click',function(e){
-			$oMaskPhoto.fadeOut();
-		});
-		
-		//删除选择图片
-		$aBtnClose.on('click',function(e){
-			$(this).closest('.photos').remove();
-			var number = parseInt($oLastPhoto.text())+1;//更改可上传图片数量
-			$oLastPhoto.text(number);
-		});
+        });
 
-	});
+        $oBtnAdd.on('click', function() {
+
+            // 拼接photo模块
+            var id = _.uniqueId('photo_');
+
+            var html = '';
+            html += '<div class="photos">';
+            html += '<a href="javascript:;" class="photo"><img src="" id="' + id + '" /></a>';
+            html += '<a href="javascript:;" class="btn-close"><img src="assets/imgs/gaw-diy/btn_close.png"/></a>';
+            html += '</div>';
+
+            var $html = $(html);
+            $html.hide();
+
+            // 追加到当前元素前面
+            $(this).before($html);
+
+            $('#uploadimg').click();
+            compressImg('uploadimg', id, 480, function(){
+            	if($oUpload.find('.photo').length >= 3){
+            		$oBtnAdd.hide();
+            	}
+            	$html.show();
+            });
+
+        });
+
+    });
 
 })(window, document);
