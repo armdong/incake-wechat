102d90d94e0161281d7efa3681cf9ba10f582855
chenxj92 <842674122@qq.com>
2016-10-10 17:45:03 +0800

    喜饼问卷活动修改

--- assets/js/cakes-questionnaire.js
+++ assets/js/cakes-questionnaire.js
@@ -1,4 +1,4 @@
-(function($) {
+(function() {
 
     $(function() {
         fnQuestionnaire();
@@ -7,7 +7,15 @@
     function fnQuestionnaire() {
         var $oForm = $('#formQuestion'),
             $aQuestion = $oForm.find('.question'),
-            iMaxLen = 200;
+            iMaxLen = 200,
+            $oBtnSubmit = $oForm.find('.btn-submit'),
+            $oResultReg = $('#result-reg'),
+            $oMaskResult = $('#mask-result'),
+	        $oMaskClose = $oMaskResult.find('.btn-result-close'),
+	        $oResultLogin = $oMaskResult.find('.result-login'),
+	        $oResultFalse = $oMaskResult.find('.result-false'),
+	        $oResultTrue = $oMaskResult.find('.result-true'),
+	        $oBtnRegister = $('#btn-register');
 
         // 答案切换事件
         $aQuestion.find('ul').on('click', 'li', function() {
@@ -36,24 +44,68 @@
         });
 
         // 表单提交
-        $oForm.find('.btn-submit').on('click', function(){
+        $oBtnSubmit.on('click', function(){
 
-        	// TODO 这里做非空验证，单选题必须选一个
-
-        	// 是否是登录状态
-        	var isLogined = false;
-
-        	if(isLogined){
-        		$oForm.find('.logined').fadeIn();
-        	} else {
-        		$oForm.find('.unlogined').fadeIn();
-        	}
+        	// TODO 非空验证，单选题必须选一个
+			var reg = $aQuestion.find('.active').length == 4 ? true : false;
+			
+			if(reg){
+				// 是否是登录状态
+	        	var isLogin = false;
+	
+				if (isLogin) {
+					checkStatus(status);
+	            }else{
+	                $oResultLogin.fadeIn(function(){
+	                    $oMaskResult.fadeIn();
+	                });
+	            }
+			}else{
+				$oResultReg.fadeIn(function(){
+                    $(this).fadeOut(2500);
+                });
+			}
+			
+        });
+        
+        $oBtnRegister.on('click', function(event) {
+            event.preventDefault();
+            /* Act on the event */
+           
+            checkStatus(status);
+            
+        });
+		
+        $oMaskClose.on('click', function(event) {
+            event.preventDefault();
+            /* Act on the event */
+            $oMaskResult.find('.result-info').hide(function(){
+                $oMaskResult.hide();
+            });
         });
 
-        // 关闭弹层
-        $oForm.find('.mask-tip').on('click', '.tip-close', function(){
-        	$(this).closest('.mask-tip').fadeOut();
-        });
+    }
+    
+    // TODO 检测是否已领取
+    function checkStatus(){
+    	var $oMaskResult = $('#mask-result'),
+	        $oResultLogin = $oMaskResult.find('.result-login'),
+	        $oResultFalse = $oMaskResult.find('.result-false'),
+	        $oResultTrue = $oMaskResult.find('.result-true'),
+	        status = true;	   // 用户是否领取 已领：true，未领：false
+    	
+    	$oResultLogin.hide();
+        //判断用户是否领取 已领：true，未领：false
+        if (status) {
+            $oResultFalse.fadeIn(function(){
+                $oMaskResult.fadeIn();
+            });
+        }else{
+        	$oResultTrue.fadeIn(function(){
+                $oMaskResult.fadeIn();
+            });
+        	
+        }
     }
 
-})(jQuery);
+})();
